0daccf9902ae3bfda843552e158e99e28747777b
Mike de Heij <mike@deheij.email>
2016-01-31 01:32:05 +0100

    Add Go files

---
+++ errors.go
@@ -0,0 +1,15 @@
+package gotools
+
+import (
+	"errors"
+)
+
+func PanicIf(err error, what ...string) {
+	if err != nil {
+		if len(what) == 0 {
+			panic(err)
+		}
+
+		panic(errors.New(err.Error() + what[0]))
+	}
+}
